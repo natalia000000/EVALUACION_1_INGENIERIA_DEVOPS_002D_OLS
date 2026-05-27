803e76df6ae3079807c2def3273e840338b7c6d0
natalia000000 <na.paredesbustamante@gmail.com>
2026-05-26 20:27:00 -0400

    evaluacion 2 devops

--- Perfulandia-Backend-Natalia/src/test/java/com/Inventario/Microservicio_Inventario/MicroservicioProductoStockApplicationTests.java
+++ Perfulandia-Backend-Natalia/src/test/java/com/Inventario/Microservicio_Inventario/MicroservicioProductoStockApplicationTests.java
@@ -3,11 +3,14 @@ package com.Inventario.Microservicio_Inventario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+  "spring.datasource.url=jdbc:h2:mem:testdb",
+  "spring.datasource.driverClassName=org.h2.Driver",
+  "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class MicroservicioProductoStockApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
